92d8e9e8dd1cf16b3098322b759f95aed6dfe934
Rozag <rozag26@mail.ru>
2014-04-18 12:46:25 +0400

    Нагрузил адаптер

--- src/com/vsrstudio/example/drawer/adapters/ContentAdapter.java
+++ src/com/vsrstudio/example/drawer/adapters/ContentAdapter.java
@@ -6,7 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.vsrstudio.example.drawer.R;
 
 public class ContentAdapter extends BaseAdapter {
@@ -14,12 +16,19 @@ public class ContentAdapter extends BaseAdapter {
     private final Context context;
     private final int section;
 
+    private final int[] images = new int[]{
+            android.R.drawable.ic_menu_compass,
+            android.R.drawable.star_on,
+            android.R.drawable.ic_dialog_email,
+            android.R.drawable.arrow_down_float,
+    };
+
     public ContentAdapter(Context context, int section) {
         this.context = context;
         this.section = section;
     }
 
-    public View getView(int position, View convertView, ViewGroup viewGroup) {
+    public View getView(final int position, View convertView, ViewGroup viewGroup) {
         LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
 
         convertView = mInflater.inflate(R.layout.grid_item, null);
@@ -34,13 +43,22 @@ public class ContentAdapter extends BaseAdapter {
                 convertView.findViewById(R.id.box).setBackground(context.getResources().getDrawable(R.drawable.box_blue));
                 break;
         }
-        ((TextView) convertView.findViewById(R.id.item_title)).setText("Position = " + position);
+        ((TextView) convertView.findViewById(R.id.large_title)).setText("Position = " + position);
+        ((TextView) convertView.findViewById(R.id.medium_title)).setText("Sub title");
+        ((TextView) convertView.findViewById(R.id.small_title)).setText("Sub sub title :)");
+        ((ImageView) convertView.findViewById(R.id.icon)).setImageResource(images[(int) (Math.random() * images.length)]);
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast.makeText(context, "You've just tapped position " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return convertView;
     }
 
     public int getCount() {
-        return 10;
+        return 20;
     }
 
     public Object getItem(int position) {
--- src/com/vsrstudio/example/drawer/fragments/ContentFragment.java
+++ src/com/vsrstudio/example/drawer/fragments/ContentFragment.java
@@ -1,6 +1,8 @@
 package com.vsrstudio.example.drawer.fragments;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +18,14 @@ public class ContentFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment, container, false);
+
+        Context context = rootView.getContext();
+        SharedPreferences prefs = context.getSharedPreferences("some preferences", Context.MODE_PRIVATE);
+        int count = prefs.getInt("counter", 0);
+        count = (count + 1) % 31415;
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("counter", count);
+        editor.commit();
 
         int position = getArguments().getInt("positions");
 
